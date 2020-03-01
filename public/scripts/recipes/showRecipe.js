31283f93f6b0f8e0c304614eea3ff88fc9f679e5
LiaTsernant <yulia.tsernant@d-d.me>
2020-02-29 22:30:14 -0800

    Add share popover functionality

--- public/scripts/recipes/showRecipe.js
+++ public/scripts/recipes/showRecipe.js
@@ -1,6 +1,6 @@
-// console.log('Show Recipe Test'); - Lia: Tested. It works!
+// console.log('Show Recipe Test')
 
-// Functionality of the modal
+// Functionality of the modal Review
 $('#exampleModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var recipient = button.data('whatever') // Extract info from data-* attributes
@@ -10,4 +10,22 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     modal.find('.modal-title').text('New message to ' + recipient)
     modal.find('.modal-body input').val(recipient)
 });
+
+// Show message "Share..." on click
+$('#popover').on('click', function () {
+    $('#popover').popover('show');
+    let message = $('#popover')[0].dataset.content;
+
+    copyToClipboard(message);
+});
+
+// Automatically copy message to clipboard
+function copyToClipboard(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
 
