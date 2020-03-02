9377ae06c3729a3474c1a44137e86d74c7f1f525
ancaagp <anca.agapi@gmail.com>
2020-03-02 13:55:43 -0800

    created api routes for recipe

--- models/index.js
+++ models/index.js
@@ -14,6 +14,7 @@ mongoose.connect(DB_URI, {
 //Export ALL models
 module.exports = {
     Recipe: require('./Recipe'),
-    // Recipe: require('./Recipe'),
     Review: require('./Review'),
+    User: require('./User'),
+
 };
