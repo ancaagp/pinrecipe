e3358bf5389f6621959c9bf98bd74981674c4e90
LiaTsernant <yulia.tsernant@d-d.me>
2020-03-02 20:11:45 -0800

    Remove duplacate data before inserting ingredients and reviews

--- public/scripts/recipes/showRecipe.js
+++ public/scripts/recipes/showRecipe.js
@@ -16,6 +16,8 @@ function getRecipe() {
 
 getRecipe();
 
+//-------------------------------------------------------------------------------- RENDER PAGE
+
 function render(recipeObj) {
     setPhotoColumn(recipeObj);
     setIngredients(recipeObj.ingredients);
@@ -23,6 +25,8 @@ function render(recipeObj) {
     provideHowToCookLink(recipeObj.link);
     renderReviews(recipeObj.reviews)
 }
+
+//-------------------------------------------------------------------------------- SET PHOTO
 
 function setPhotoColumn(recipeObj) {
     console.log(recipeObj);
@@ -34,20 +38,28 @@ function setPhotoColumn(recipeObj) {
     // console.log(popoverPlaceholder);  
 };
 
+//-------------------------------------------------------------------------------- SET INGREDIENTS
+
 function setIngredients(array) {
     const ingredientsUl = document.getElementById('ingredients');
-
-    for (let i = 0; i < array.length; i += 1) {
-        let li = document.createElement('li');
-        li.textContent = array[i];
-        ingredientsUl.appendChild(li);
+    if (ingredientsUl.childNodes.length === 0) {
+        for (let i = 0; i < array.length; i += 1) {
+            let li = document.createElement('li');
+            li.textContent = array[i];
+            ingredientsUl.appendChild(li);
+        };
     };
 };
+
+//-------------------------------------------------------------------------------- SET DESCRIPTION
 
 function setDescription(description) {
     const descriptionPlaceholder = document.getElementById('description')
     descriptionPlaceholder.textContent = description;
 };
+
+//-------------------------------------------------------------------------------- SET LINK
+
 
 function provideHowToCookLink(strLink) {
     const howToCook = document.getElementById('link');
@@ -55,24 +67,27 @@ function provideHowToCookLink(strLink) {
     howToCook.setAttribute('target', '_blank');
 };
 
+//-------------------------------------------------------------------------------- DISPLAY REVIEWS
 
 function renderReviews(reviewsArray) {
-    let reviewUl = document.getElementById('reviews');
-
-    console.log(`REVIEWS ${reviewUl.childNodes}`);
-    
+    let reviewUl = document.getElementById('reviewsUl');
+    while(reviewUl.childNodes.length) {
+        reviewUl.removeChild(reviewUl.childNodes[reviewUl.childNodes.length - 1]);
+    };    
 
     for (let i = 0; i < reviewsArray.length; i += 1) {
         let review = `
         <li class="d-flex align-items-start m-4">
-            ${reviewsArray[i].body}
             <i class="fa fa-edit"></i>
+            ${reviewsArray[i].body}
             <i class="fa fa-remove" style="color: red"></i>
         </li>
         `
-        reviewUl.insertAdjacentHTML('beforeend', review);
+        reviewUl.insertAdjacentHTML('afterbegin', review);
     };
 };
+
+//-------------------------------------------------------------------------------- ADD NEW REVIEW
 
 const reviewForm = document.getElementById('review-modal');
 reviewForm.addEventListener('submit', (event) => {
@@ -84,6 +99,7 @@ reviewForm.addEventListener('submit', (event) => {
 
     if (body.value.length > 0) {
         const newReview = { body: body.value };
+        document.getElementById('review-text').value = "";
         // console.log(newReview);
 
         fetch(`/api/v1/recipes/${recipeId}/reviews`, {
@@ -102,18 +118,7 @@ reviewForm.addEventListener('submit', (event) => {
     };
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+//------------------------------------------------------------------------------------------------- OPEN REVIEW MODAL
 
 // Functionality of the modal Review
 $('#exampleModal').on('show.bs.modal', function (event) {
@@ -126,7 +131,7 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     modal.find('.modal-body input').val(recipient)
 });
 
-// Show message "Share..." on click
+//------------------------------------------------------------------------------------------------- Show message "Share..." on click
 $('#popover').on('click', function () {
     $('#popover').popover('show');
     let message = $('#popover')[0].dataset.content;
@@ -134,7 +139,7 @@ $('#popover').on('click', function () {
     copyToClipboard(message);
 });
 
-// Automatically copy message to clipboard
+//------------------------------------------------------------------------------------------------- Automatically copy message to clipboard
 function copyToClipboard(str) {
     const el = document.createElement('textarea');
     el.value = str;
