b4c12d0ce1e9c942a4417be9c0c361181a9a6e95
ancaagp <anca.agapi@gmail.com>
2020-03-04 10:04:28 -0800

    added comment for testing

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./Review');
 
+// deleted method and kept link
 const RecipeSchema = new Schema ({
     name: String,
     description: String,
